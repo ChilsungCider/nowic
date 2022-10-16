3055c5a6e62790169a52fe9e3741c2a365e3aace
ChilsungCider <22100487@handong.ac.kr>
2022-10-16 20:48:45 +0900

    update
    update

--- psets/pset7/infixall.cpp
+++ psets/pset7/infixall.cpp
@@ -12,7 +12,6 @@
 //   1 + (((12 * 2) / 4) - 1)
 //
 // Author: idebtor@gmail.com
-// 2020.04.05	created and assert() added
 //
 #include <iostream>
 #include <cassert>
@@ -29,47 +28,36 @@ using namespace std;
 // a basic stack functinality only provided for pedagogical purpose only.
 #include <vector>
 template <typename T>
-struct stack
-{
-	vector<T> item;
+struct stack {
+    vector<T> item;
 
-	bool empty() const
-	{
-		return item.empty();
-	}
-	auto size() const
-	{
-		return item.size();
-	}
-	void push(T const &data)
-	{
-		item.push_back(data);
-	}
-	void pop()
-	{
-		if (item.empty())
-			throw out_of_range("stack<>::pop(): pop stack");
-		item.pop_back();
-	}
-	T top() const
-	{
-		if (item.empty())
-			throw out_of_range("stack<>::top(): top stack");
-		return item.back();
-	}
+    bool empty() const {
+        return item.empty();
+    }
+    auto size() const {
+        return item.size();
+    }
+    void push(T const& data) {
+        item.push_back(data);
+    }
+    void pop() {
+        if (item.empty())
+            throw out_of_range("stack<>::pop(): pop stack");
+        item.pop_back();
+    }
+    T top() const {
+        if (item.empty())
+            throw out_of_range("stack<>::top(): top stack");
+        return item.back();
+    }
 };
 #else /////////////////////////// using STL stack //////////////////////////
 #include <stack>
-#include <cmath>
 #endif ///////////////////////////////////////////////////////////////////////
 
 template <typename T>
 void printStack(stack<T> orig)
 {
-
-	// cout << "Step 2: your code here: copy printStack() template version from postfix.cpp\n";
-	// cout << "Step 3: rewrite printStack() using recursion. refer to PDF file\n";
-
 	if (orig.size() == 1)
 	{
 		cout << orig.top() << ' ';
@@ -81,8 +69,25 @@ void printStack(stack<T> orig)
 		printStack(orig);
 		orig.push(temp);
 	}
+	// cout << endl;
+}
 
-	cout << endl;
+int precedence(char op)
+{
+	if (op == '+')
+		return 1;
+	else if (op == '-')
+		return 1;
+	else if (op == '*')
+		return 2;
+	else if (op == '/')
+		return 2;
+	else if (op == '^')
+		return 3;
+	else if (op == '(')
+		return 0;
+	else
+		return 10;
 }
 
 // performs arithmetic operations.
@@ -99,22 +104,29 @@ double apply_op(double a, double b, char op)
 	case '/':
 		return a / b;
 	case '^':
-		return pow(a, b);
+	{
+		int j = a;
+		for (int i = 0; i < b - 1; i++)
+		{
+			a *= j;
+		}
+		return a;
+	}
 	}
 	cout << "Unsupported operator encountered: " << op << endl;
 	return 0;
 }
 
-// there is a bug...
+// there is a bug... bug: int -> double , right first
 double compute(stack<double> &va_stack, stack<char> &op_stack)
 {
-	double left = va_stack.top();
-	va_stack.pop();
 	double right = va_stack.top();
+	va_stack.pop();
+	double left = va_stack.top();
 	va_stack.pop();
 	char op = op_stack.top();
 	op_stack.pop();
-	double value = apply_op(right, left, op);
+	double value = apply_op(left, right, op);
 
 	DPRINT(cout << " va/op_stack.pop: " << value << endl;);
 	return value;
@@ -127,94 +139,152 @@ double evaluate(string tokens)
 	stack<double> va_stack; // stack to store operands or values
 	stack<char> op_stack;	// stack to store operators.
 	double value = 0;
-	int n = 1;
+	int k = 1;
 
 	for (size_t i = 0; i < tokens.length(); i++)
 	{
 		// token is a whitespace or an opening brace, skip it.
-		if (isspace(tokens[i]) || tokens[i] == '(')
+		if (isspace(tokens[i]))
 			continue;
+
 		DPRINT(cout << " tokens[" << i << "]=" << tokens[i] << endl;);
 
 		// current token is a value(or operand), push it to va_stack.
 		if (isdigit(tokens[i]))
 		{
 			int ivalue = 0;
-			// cout << "your code here: to handle multi-digits(operand)\n";
 			ivalue = tokens[i] - '0';
-			if (n == 1)
+			// cout << "your code here: to handle multi-digits(operand)\n";
+
+			if (k == 1)
 			{
 				va_stack.push(ivalue);
-				n++;
+				k++;
 			}
+
 			else
 			{
 				double temp1 = va_stack.top();
 				va_stack.pop();
 				double temp2 = ivalue + temp1 * 10;
 				va_stack.push(temp2);
-				n++;
+				k++;
 			}
 		}
+		else if (tokens[i] == '(')
+		{
+			op_stack.push(tokens[i]);
+			op_stack.top();
+		}
+
 		else if (tokens[i] == ')')
 		{ // compute it, push the result to va_stack
-			value = compute(va_stack, op_stack);
-			va_stack.push(value);
-			n = 1;
+			// do
+			// {
+			//     value = compute(va_stack, op_stack);
+			//     va_stack.push(value);
+			// } while (op_stack.top() == '(');
+			// op_stack.pop();
+			int t = op_stack.size();
+			for (int i = 0; i < t; i++)
+			{
+				if (op_stack.top() == '(')
+				{
+					op_stack.top();
+					op_stack.pop();
+					break;
+				}
+				va_stack.push(compute(va_stack, op_stack));
+			}
+
+			k = 1;
 		}
 		else
 		{ // token is an operator; push it to op_stack.
-			op_stack.push(tokens[i]);
-			n = 1;
+			if (op_stack.empty() || op_stack.top() == '(')
+			{
+				op_stack.size();
+				op_stack.push(tokens[i]);
+			}
+			else
+			{
+				if (precedence(tokens[i]) >= precedence(op_stack.top()))
+				{
+					op_stack.push(tokens[i]);
+				}
+				else
+				{
+					va_stack.push(compute(va_stack, op_stack));
+					op_stack.push(tokens[i]);
+				}
+			}
+
+			k = 1;
 		}
 	}
-
-	for (int i = 0; i < op_stack.size(); i++)
+	int l = 0;
+	l = op_stack.size();
+	for (int i = 0; i < l; i++)
 	{
-		char ot = op_stack.top();
+		char op = op_stack.top();
 		op_stack.pop();
 		double temp1 = va_stack.top();
 		va_stack.pop();
 		double temp2 = va_stack.top();
 		va_stack.pop();
 
-		if (ot == '+')
+		switch (op)
 		{
-			va_stack.push(temp2 + temp1);
+		case '+':
+		{
+			value = temp2 + temp1;
+			va_stack.push(value);
 			break;
 		}
-		else if (ot == '-')
+		case '-':
 		{
-			va_stack.push(temp2 - temp1);
+			value = temp2 - temp1;
+			va_stack.push(value);
 			break;
 		}
-		else if (ot == '*')
+		case '*':
 		{
-			va_stack.push(temp2 * temp1);
+			value = temp2 * temp1;
+			va_stack.push(value);
 			break;
 		}
-		else if (ot == '/')
+		case '/':
 		{
-			va_stack.push(temp2 / temp1);
+			value = temp2 / temp1;
+			va_stack.push(value);
 			break;
 		}
-		else if (ot == '^')
+		case '^':
 		{
-			va_stack.push(pow(temp2, temp1));
+			value = 1;
+			for (int i = 0; i < temp2; i++)
+			{
+				value *= temp1;
+			}
+			va_stack.push(value);
 			break;
 		}
-		else
+		default:
 			break;
+		}
 	}
-
 	DPRINT(cout << "tokens exhausted: now, check two stacks:" << endl;);
 	DPRINT(printStack(va_stack); cout << endl;);
 	DPRINT(printStack(op_stack); cout << endl;);
 
-	// assert(op_stack.empty());
-	if (!op_stack.empty())
-		printf("\n");
+	// cout << "your code here: process if !op_stack.empty() \n";
+	assert(va_stack.size() == 1);
+	assert(op_stack.empty());
+
+	// cout << "your code here: post-conditions\n";
 	value = va_stack.top();
+
+	// cout << "your code here: return & clean-up\n";
 	va_stack.pop();
 
 	return value;
